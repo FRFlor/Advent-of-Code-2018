4b19f187b0f9215db7f4714f3e5580d63ae4c410
Felipe Flor <f.flor@vehikl.com>
2018-12-04 13:40:27 -0500

    Solve first start for Challenge 4

--- AdventOfCode.php
+++ AdventOfCode.php
@@ -201,5 +201,142 @@ class DayThree
     }
 }
 
-$master = new DayThree();
-var_dump($master->secondStar());
+class DayFour {
+    private $input;
+    static $guardsSchedule = []; // Using static so the data won't be allocated in Heap Memory nor the Stack, but rather in the Data Segment
+
+    public function __construct()
+    {
+        $this->input = getDayFourInputs();
+        $this->sortGuardsShifts();
+    }
+
+    public function firstStar()
+    {
+        $guardsSleepAmount = $this->getMinutesSleptByEachGuard();
+
+        // Find the Guard that has slept the most
+        $greatestSleep = 0;
+        $greatestSleeper = null;
+        foreach ($guardsSleepAmount as $guardId => $sleepTime) {
+            if ($sleepTime > $greatestSleep) {
+                $greatestSleeper = $guardId;
+                $greatestSleep = $sleepTime;
+            }
+        }
+
+        // Find the minute most slept by the guard
+        $minutesArray = $this->getSleepDistributionForGuard($greatestSleeper);
+        $highestCount = 0;
+        $mostSleptMinute = null;
+        foreach ($minutesArray as $minute => $count) {
+            if ($count > $highestCount) {
+                $mostSleptMinute = $minute;
+                $highestCount = $count;
+            }
+        }
+
+        return $mostSleptMinute * $greatestSleeper;
+    }
+
+    protected function getMinutesDifference($date1, $date2)
+    {
+        $interval = $date1->diff($date2);
+
+        return $interval->h * 60 + $interval->i;
+    }
+
+    protected function sortGuardsShifts()
+    {
+        foreach ($this->input as $line) {
+            array_push(self::$guardsSchedule, $this->parseInputLine($line));
+        }
+
+        usort(self::$guardsSchedule, function ($a, $b) {
+            return $b[0] < $a[0];
+        });
+    }
+
+    protected function getSleepDistributionForGuard($guardId)
+    {
+        $minutes = [];
+        $startToSleep = null;
+        $wakesUp = null;
+
+        $isRightGuard = false;
+        foreach (self::$guardsSchedule as $entry) {
+            if (strpos($entry[1], 'Guard') !== false) {
+                preg_match("/Guard #(\d+) .+/", $entry[1], $matches);
+                $thisGuardId = (int) $matches[1];
+                $isRightGuard = ($thisGuardId === $guardId);
+            }
+            elseif (strpos($entry[1], 'falls') !== false && $isRightGuard) {
+                $startToSleep = $entry[0];
+            }
+            elseif (strpos($entry[1], 'wakes') !== false && $isRightGuard) {
+                $wakesUp = $entry[0];
+
+                $startHour = (int) $startToSleep->format("H");
+                $startMinute = (int) $startToSleep->format("i");
+                $endHour = (int) $wakesUp->format("H");
+                $endMinute = (int) $wakesUp->format("i");
+
+                $hour = $startHour;
+                $minute =  $startMinute;
+                while ($hour !== $endHour || $minute !== $endMinute) {
+                    $minutes[$minute] = ($minutes[$minute] ?? 0) + 1;
+
+                    // Increment Time
+                    $minute++;
+                    if ($minute > 59) {
+                        $hour++;
+                        $minute = 0;
+                        if ($hour > 23) {
+                            $hour = 0;
+                            $minute = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        return $minutes;
+
+    }
+
+    protected function getMinutesSleptByEachGuard()
+    {
+        $guardId = null;
+        $startToSleep = null;
+        $wakesUp = null;
+
+        $guardsSleepTrack = [];
+        foreach (self::$guardsSchedule as $entry) {
+            if (strpos($entry[1], 'Guard') !== false) {
+                preg_match("/Guard #(\d+) .+/", $entry[1], $matches);
+                $guardId = $matches[1];
+            }
+            elseif (strpos($entry[1], 'falls') !== false) {
+                $startToSleep = $entry[0];
+            }
+            elseif (strpos($entry[1], 'wakes') !== false) {
+                $wakesUp = $entry[0];
+                $guardsSleepTrack[$guardId] = ($guardsSleepTrack[$guardId] ?? 0) + $this->getMinutesDifference($startToSleep, $wakesUp);
+            }
+        }
+
+        return $guardsSleepTrack;
+    }
+
+    protected function parseInputLine($line)
+    {
+        preg_match("/\[(.+)\] (.+)/", $line, $matches);
+        $time = new DateTime($matches[1]);
+        $event = $matches[2];
+
+        return [$time, $event];
+    }
+}
+
+$master = new DayFour();
+var_dump($master->firstStar());
