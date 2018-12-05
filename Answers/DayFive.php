77032d74b0cece45deb73ecaf0b43bec98f569a9
Felipe Flor <f.flor@vehikl.com>
2018-12-05 10:31:16 -0500

    Solve Day 5 first star with Linked Lists
    - This has reduced the execution time of the solution in 3!

--- Answers/DayFive.php
+++ Answers/DayFive.php
@@ -6,45 +6,118 @@ class DayFive
 
     public function __construct()
     {
-        $this->chain = str_split(getDayFiveInputs());
-        array_push($this->chain, NULL);
+        $this->chain = new Chain(getDayFiveInputs());
     }
 
     public function firstStar()
     {
-        $i = 0;
-        $reactionsCount = 0;
         do {
-            $reactionsInLoop = 0;
-            while ($this->chain[$i+1] !== NULL) {
-                if ($this->areCharsReactive($this->chain[$i], $this->chain[$i + 1])) {
-                    $this->reactAt($i);
-                    $reactionsInLoop++;
-                } else {
-                    $i++;
-                }
-            }
-            $i = 0;
-            $reactionsCount += $reactionsInLoop;
-        } while($reactionsInLoop !== 0);
+            $reactionsInLoop = $this->chain->reactiveTransverse();
+        } while ($reactionsInLoop !== 0);
 
-        return count($this->chain) - 1;
+        return $this->chain->count();
     }
-
-    public function secondStar()
-    {
-    }
-
-    // Returns true if $a and $b react with each other
-    public function areCharsReactive($a, $b)
-    {
-        return abs(ord($a) - ord($b)) === 32;
-    }
-
-    public function reactAt($index)
-    {
-        array_splice($this->chain, $index, 2);
-    }
-
 }
 
+
+class Unit {
+    public $value;
+    public $next;
+    public $prev;
+
+    public function __construct($char)
+    {
+        $this->value = ord($char);
+        $this->next = null;
+        $this->prev = null;
+    }
+
+    public function reactsWithNext()
+    {
+        return abs($this->next->value - $this->value) === 32;
+    }
+}
+
+class Chain {
+    private $start;
+    private $end;
+    private $count;
+
+    public function __construct(string $source)
+    {
+        $this->count = 0;
+
+        $this->attachFirst($source[0]);
+
+        for ($i = 1; $i < strlen($source); $i++){
+            $this->attach($source[$i]);
+        }
+    }
+
+    public function attachFirst(string $char)
+    {
+        $firstUnit = new Unit($char);
+        $this->start = $firstUnit;
+        $this->end = $firstUnit;
+        $this->count++;
+    }
+
+    public function attach(string $char)
+    {
+        $unit = new Unit($char);
+        $unit->prev = $this->end;
+        $this->end->next = $unit;
+        $this->end = $unit;
+        $this->count++;
+    }
+
+    public function detach(Unit $unit)
+    {
+        $previousUnit = $unit->prev;
+        $nextUnit = $unit->next;
+
+        // If the unit was the first in the chain
+        if ($previousUnit === null) {
+            $nextUnit->prev = null;
+        }
+        // If the unit was the last in the chain
+        elseif ($nextUnit === null) {
+            $previousUnit->next = null;
+        }
+        // If the unit was not in one of the extremes
+        else {
+            $previousUnit->next = $nextUnit;
+            $nextUnit->prev = $previousUnit;
+        }
+
+        unset($unit);
+        $this->count--;
+    }
+
+    public function reactiveTransverse()
+    {
+        $reactionsCount = 0;
+        $currentUnit = $this->start;
+
+        do {
+            if ($currentUnit->reactsWithNext()) {
+                $nextTarget = $currentUnit->next->next;
+                $this->detach($currentUnit->next);
+                $this->detach($currentUnit);
+
+                $reactionsCount++;
+            } else {
+                $nextTarget = $currentUnit->next;
+            }
+
+            $currentUnit = $nextTarget;
+        } while ($currentUnit->next !== null);
+
+        return $reactionsCount;
+    }
+
+    public function count()
+    {
+        return $this->count;
+    }
+}
