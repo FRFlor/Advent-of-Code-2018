3097c14ac66da17d60ecbd1b49212ba2038dfdfb
Felipe Flor <f.flor@vehikl.com>
2018-12-11 21:23:07 -0500

    Refactor into using Binary Search

--- Answers-In-Javascript/DayTen.js
+++ Answers-In-Javascript/DayTen.js
@@ -1,75 +1,98 @@
 class Star {
     constructor(x, y, vx, vy) {
-        this.position = { x: parseInt(x), y: parseInt(y) };
-        this.speed = { x: parseInt(vx), y: parseInt(vy)};
+        this.x0 = parseInt(x);
+        this.y0 = parseInt(y);
+
+        this.position = {x: this.x0, y: this.y0 };
+        this.speed = {x: parseInt(vx), y: parseInt(vy)};
     }
 
-    update(){
-        this.position.x += this.speed.x;
-        this.position.y += this.speed.y;
+    update(seconds) {
+        this.position.x = this.x0 + this.speed.x * seconds;
+        this.position.y = this.y0 + this.speed.y * seconds;
     }
 }
+
 
 class DayTen {
     constructor(input) {
         this.stars = new Array(input.length);
+        this.area = null;
         this.seconds = 0;
         for (let i = 0; i < input.length; i++) {
-            const m = input[i].match(/position=<\s*(\-?\d+),\s*(\-?\d+)> velocity=<\s*(\-?\d+),\s*(\-?\d+)>/);
-            this.stars[i] = new Star(m[1]-1, m[2]-1, m[3], m[4]);
+            const m = input[i].match(/position=<(.+),(.+)> velocity=<(.+),(.+)>/);
+            this.stars[i] = new Star(m[1] - 1, m[2] - 1, m[3], m[4]);
         }
     }
 
-    update() {
-        this.seconds++;
-        for(let i = 0; i < this.stars.length; i ++) {
-            this.stars[i].update();
-        }
-    }
-
-    // The message guaranteed to not be ready if there are rogue stars still.
-    // A rogue star being a star that is alone in its row.
-    isMessageReady() {
+    update(newSeconds) {
+        this.seconds = newSeconds;
         for (let i = 0; i < this.stars.length; i++) {
-            let star = this.stars[i];
-            if (this.stars.filter( matches => matches.position.y === star.position.y).length === 1) {
-                return false;
-            }
+            this.stars[i].update(newSeconds);
         }
 
-        return true;
+        this.calculateEdges();
+        this.area = ( this.maxY - this.minY ) * ( this.maxX - this.minX );
     }
 
     calculateEdges() {
-        let coordsX = this.stars.map( star => star.position.x );
-        let coordsY = this.stars.map( star => star.position.y );
+        let coordsX = this.stars.map(star => star.position.x);
+        let coordsY = this.stars.map(star => star.position.y);
 
-        this.minX =  Math.min(...coordsX);
-        this.minY =  Math.min(...coordsY);
-        this.maxX =  Math.max(...coordsX);
-        this.maxY =  Math.max(...coordsY);
+        this.minX = Math.min(...coordsX);
+        this.minY = Math.min(...coordsY);
+        this.maxX = Math.max(...coordsX);
+        this.maxY = Math.max(...coordsY);
     }
 
     render() {
         this.calculateEdges();
 
-        let result = "\n";
+        let result = '\n';
         for (let y = this.minY; y <= this.maxY; y++) {
-            let line = "";
+            let line = '';
             for (let x = this.minX; x <= this.maxX; x++) {
-                let star = this.stars.find( star => star.position.x === x && star.position.y === y);
-                line += (star === undefined) ? '.' : '#';
+                let star = this.stars.find(star => star.position.x === x && star.position.y === y);
+                line += ( star === undefined ) ? '.' : '#';
             }
-            result += line + "\n";
+            result += line + '\n';
         }
 
         return result;
     }
 
+    convergeToMessage(t0, tf) {
+        let midpointT = Math.floor((t0 + tf)/2);
+        this.update(midpointT);
+        let midpointA = this.area;
+        this.update(midpointT + 1);
+        let nextPointA = this.area;
+        let isDispersing = (nextPointA > midpointA);
+
+
+        if (tf - t0 < 5) {
+            // The change is small enough that it is okay to transverse one by one now
+            let delta = isDispersing ? -1 : 1;
+            let lastArea = midpointA;
+            let newArea = nextPointA;
+            do {
+                lastArea = newArea;
+                this.update(this.seconds + delta);
+                newArea = this.area;
+            } while (newArea < lastArea);
+
+            return this.update(this.seconds - delta);
+        }
+
+        if (isDispersing) {
+            return this.convergeToMessage(t0, midpointT);
+        } else {
+            return this.convergeToMessage(midpointT, tf);
+        }
+    }
+
     firstStar() {
-        do {
-            this.update();
-        } while (! this.isMessageReady());
+        this.convergeToMessage(0, 1000000);
 
         return this.render();
     }
@@ -80,10 +103,10 @@ class DayTen {
 }
 
 
-
 const Support = require('./Support.js');
 Support.Timer(() => {
-    let dayEight = new DayTen(require('./DayTenInput'));
-    console.log('First Star: ' + dayEight.firstStar());
-    console.log('Second Star: ' + dayEight.secondStar());
+    let dayTen = new DayTen(require('./DayTenInput'));
+
+    console.log('First Star: ' + dayTen.firstStar());
+    console.log('Second Star: ' + dayTen.secondStar());
 });
